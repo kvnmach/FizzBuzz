0accbd951156aeaf273dd7a00ae21abd45b807d4
kvnmach <kvnmach@gmail.com>
2016-03-19 12:55:52 -0500

    else if statements

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            for (int index = 1; index <= 100; index++)
+
+            {
+                if (index%3 == 0 && index%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (index%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (index%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(index);
+                }
+                
+            }
+            Console.ReadKey();
         }
     }
 }
